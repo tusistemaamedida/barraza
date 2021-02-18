68b00a12bc377667137c4bda9fcbdf203c56c22a
Torres <torrespjgt@gmail.com>
2021-02-18 10:12:18 -0600

    add new features

--- backend/controllers/supplies.js
+++ backend/controllers/supplies.js
@@ -1,15 +1,15 @@
+const moment = require('moment');
 const Supplies = require('../models/supplies');
 
 async function getAllSuppliess(req){
   let {pagination}= req.body
-  return await Supplies.find().skip(pagination.from).limit(pagination.limit)
+  return await Supplies.find({deleted_at: null}).skip(pagination.from).limit(pagination.limit)
 }
 
 async function getSuppliesById(id)
 {
-  return await Supplies.find({_id: id})
+  return await Supplies.find({_id: id, deleted_at : null})
 }
-
 
 async function save(body){
   let supplie = new Supplies({
@@ -18,13 +18,18 @@ async function save(body){
   return await supplie.save()
 }
 
+async function update(body, id){
+return await Supplies.updateOne({_id: id, deleted_at:null}, body)
+}
 
-async function deleted(body){
-  
+async function deleted(id){
+  return await Supplies.updateOne({_id: id}, {deleted_at: moment().format("YYYY-MM-DD hh:mm:ss")})
 }
 
 module.exports ={
   getAllSuppliess,
   save,
-  getSuppliesById
+  getSuppliesById,
+  update,
+  deleted
 }
--- backend/routes/supplies.js
+++ backend/routes/supplies.js
@@ -3,25 +3,24 @@ const express = require('express');
 const routes = express.Router()
 const { pagination } = require('../middlewares/pagination');
 const ctrSupplies = require('../controllers/supplies')
+const util = require('../controllers/util')
 
 routes.get('/:id', async (req, resp)=>{
   try {
     let {id}= req.params
     let data = await ctrSupplies.getSuppliesById(id)
-    resp.status(200).send({data:data})
+    resp.status(200).send(util.getSuccessMsg(data, 200))
   } catch (error) {
-    console.log(error)
-    resp.status(500).send({msg:"there is a problem"})
+    resp.status(500).send(util.getErrorMsg(error))
   }
 })
 
 routes.get('/', pagination, async (req, resp)=>{
   try {
     let data = await ctrSupplies.getAllSuppliess(req)
-    resp.status(200).send({data:data})
+    resp.status(200).send(util.getSuccessMsg(data, 200))
   } catch (error) {
-    console.log(error)
-    resp.status(500).send({msg:"there is a problem"})
+    resp.status(500).send(util.getErrorMsg(error))
   }
 })
 
@@ -29,10 +28,38 @@ routes.post('/', async (req, resp)=>{
   let {body} = req
   try {
     let data = await ctrSupplies.save(body)
-    resp.status(201).send(data)
+    resp.status(200).send(util.getSuccessMsg(data, 201))
   } catch (error) {
-    console.log(error)
-    resp.status(500).send({msg:"there is a problem"})
+    resp.status(500).send(util.getErrorMsg(error))
+  }
+})
+
+routes.put('/:id', async (req, resp)=>{
+  let {body} = req
+  let {id}= req.params
+  try {
+    let data = await ctrSupplies.update(body, id)
+    console.log(data)
+    if(data.nModified=== 0){
+      return resp.status(200).send(util.getSuccessMsg("Register not found", 200))
+    }
+      return resp.status(200).send(util.getSuccessMsg("Register updated", 201))
+  } catch (error) {
+    resp.status(500).send(util.getErrorMsg(error))
+  }
+})
+
+routes.delete('/:id', async (req, resp)=>{
+  let {id}= req.params
+  try {
+    let data = await ctrSupplies.deleted(id)
+    console.log(data)
+    if(data.nModified=== 0){
+      return resp.status(200).send(util.getSuccessMsg("Register not found", 200))
+    }
+    return resp.status(200).send(util.getSuccessMsg("Register deleted", 201))
+  } catch (error) {
+    resp.status(500).send(util.getErrorMsg(error))
   }
 })
 
